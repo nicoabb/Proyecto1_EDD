841e26d5e9a607d5f00c92adff4e149c9f898a0f
Nicolás Briceño <nicolas.briceno@correo.unimet.edu.ve>
2021-06-07 23:28:04 -0400

    readtxt optimizado

--- src/proyecto1_edd/DataManage.java
+++ src/proyecto1_edd/DataManage.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.*;
 
 /**
  * 
@@ -23,36 +24,19 @@ public class DataManage {
             if(empty){
                 System.out.println("Archivo creado");
             }else{
+                //lectura completa de archivo
+                byte[] alltxt = Files.readAllBytes(Paths.get("Test Packages/almacenes.txt"));
+                String info = new String(alltxt);
                 
-                //Primera lectura para obtener cantidad de líneas
-                FileReader info = new FileReader(file);
-                BufferedReader text = new BufferedReader(info);
-                int txtsize = 0; //cantidad de lineas
-                String line;
-                while((line = text.readLine()) != null){
-                    txtsize += 1;
-                }
-                text.close();
-                
-                //Segunda lectura para convertir txt a String 
-                FileReader info2 = new FileReader(file);
-                BufferedReader text2 = new BufferedReader(info2);
-                String[] content = new String[txtsize];
-                for(int i = 0; (line = text2.readLine()) != null; i++){
-                    content[i] = line;
-                }
-                info2.close();
-                String textocompleto = String.join("\n", content);
-                
-                //Aqui comienza la división de la información para convertir a lista
-                String[] infoorder = textocompleto.split(";");
+                //División del texto para enviar a grafo
+                String[] infoorder = info.split(";");
                 Lista almacenes = new Lista();
                 for(int i = 0; i < infoorder.length; i++){
                     if(infoorder[i].contains("Almacen ")){
                         String[] data = infoorder[i].split(":");
-                        almacenes.insertarAlmacen(data[0]);
+                        almacenes.insertarAlmacen(data[0]); //creacion almacenes
                         String[] products = data[1].split("\n");
-                        for(int n = 1; n <= (products.length-1); n++){
+                        for(int n = 1; n <= (products.length-1); n++){ //creacion productos
                             String[] atr = products[n].split(",");
                             //atr por los atributos del producto (nombre, cantidad)
                             almacenes.AgregarProducto(n-1, atr[0], Integer.parseInt(atr[1]));
@@ -60,7 +44,7 @@ public class DataManage {
                     }else{
                         if(infoorder[i].contains("Rutas")){
                             String[] rutas = infoorder[i+1].split("\n");
-                            for(int m = 1; m <= (rutas.length-1); m++){
+                            for(int m = 1; m <= (rutas.length-1); m++){ //creacion rutas
                                 almacenes.EvaluarRutas(almacenes.getpFirst(), rutas[m]);
                             }
                         }
@@ -69,7 +53,6 @@ public class DataManage {
             }
         }catch (NumberFormatException | IOException e){
             System.out.println("No funciona");
-        }
-        
+        }  
     }
 }
