d926858e0245910f8619633616ad6c664d0d2ee3
Gabriella Suarez <71653617+g-suarez@users.noreply.github.com>
2021-06-12 00:46:33 -0400

    pensaba que te habia olvidado pero pusieron la cancion

---
+++ src/proyecto1_edd/Cola.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_edd;
+
+/**
+ *
+ * @author gabriellasuarez
+ */
+public class Cola {
+    private Nodo first;
+    private Nodo last;
+    private int size;
+    public Cola(){
+        this.first = null;
+        this.last = null;
+        this.size= 0;
+    }
+
+    /**
+     * @return the first
+     */
+    public Nodo getFirst() {
+        return first;
+    }
+
+    /**
+     * @param first the first to set
+     */
+    public void setFirst(Nodo first) {
+        this.first = first;
+    }
+
+    /**
+     * @return the last
+     */
+    public Nodo getLast() {
+        return last;
+    }
+
+    /**
+     * @param last the last to set
+     */
+    public void setLast(Nodo last) {
+        this.last = last;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public void dequeue(){
+        first = first.getSiguiente();
+    }
+    public void queue(Nodo a){
+        if (size != 0){
+            
+        }
+    }
+}
+}
