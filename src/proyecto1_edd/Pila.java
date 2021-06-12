006bfa549b32eafd44038e6f363de23957f459de
Gabriella Suarez <71653617+g-suarez@users.noreply.github.com>
2021-06-12 00:40:59 -0400

    ALO ALGUIEN ME OYE

--- src/proyecto1_edd/Pila.java
+++ src/proyecto1_edd/Pila.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package proyecto1_edd;
 
 /**
@@ -7,4 +11,98 @@ package proyecto1_edd;
  */
 public class Pila {
     
+
+    private Nodo cima;
+    private Nodo base;
+    private int size;
+
+    public Pila() {
+        this.size = 0;
+        this.base = null;
+        this.cima = null;
+    }
+
+    public void pile(Nodo a) {
+        if (a != null) {
+            Nodo b = new Nodo(); 
+            b.setEdges(a.getEdges()); 
+            if (getSize() == 0) {
+                setCima(b);
+                setBase(b);
+            } else {
+                getCima().setSiguiente(b);
+                b.setAnterior(getCima());
+                this.setCima(b);
+            }
+            size++;
+
+        }
+    }
+
+    public void depile() {
+        if (getSize() != 0) {
+            this.setCima(getCima().getAnterior());
+        }
+        size--;
+    }
+
+    /**
+     * @return the cima
+     */
+    public Nodo getCima() {
+        return cima;
+    }
+
+    /**
+     * @param cima the cima to set
+     */
+    public void setCima(Nodo cima) {
+        this.cima = cima;
+    }
+
+    /**
+     * @return the base
+     */
+    public Nodo getBase() {
+        return base;
+    }
+
+    /**
+     * @param base the base to set
+     */
+    public void setBase(Nodo base) {
+        this.base = base;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean in(String search) {
+        boolean innit = false;
+        Nodo aux = base;
+        while ((aux != null) && !(innit)) {
+            if (aux.getNombre().equals(search)) {
+                innit = true;
+                break;
+            } else {
+                aux = aux.getSiguiente();
+            }
+        }
+
+        return innit;
+    }
 }
+
+    
+
